78f5f8d1a6145744e87170fffd21901f91839d0b
alifikrii <andimuhammadalifikri@apps.ipb.ac.id>
2021-09-12 11:02:53 +0800

    menambah fitur exit dan CD

--- sh.c
+++ sh.c
@@ -1,5 +1,5 @@
 /*
-Shell
+00ell
 =====
 
 Shell adalah program yang membuat proses child untuk menjalankan string perintah yang diberikan.
@@ -38,17 +38,37 @@ int main()
     while (1)
     {
         //  cetak prompt "$ " 
+        printf("kelompok_SO_Pertemuan4:~ $ ");
 
         //  baca string perintah
+        fgets(cmd, sizeof cmd, stdin);
 
-        //  pecah string perintah per argumen
+        //  pecah string perintah per argumen0
+        split_words(cmd, args);
 
         //  jika perintah = "exit"
         //      break
+        if (!strcmp(args[0], "exit"))
+			break;
 
         //  jika perintah = "cd"
+        if (!strcmp(args[0], "cd")) {
+			int val;
+			if (args[1]) {
+				char *dir = args[1];
+				val = chdir(dir);
+				if (val == -1) {
+					puts("No such file or directory");
+				}
+			}
+			else {
+                continue;
+			}
+		}
+
         //      ganti direktori
         //      continue
+
 
         //  buat proses child:
         //      exec string perintah
@@ -56,6 +76,8 @@ int main()
         //      keluar dengan kode -1
 
         //  tunggu child selesai
+
+        
     }
 
     return 0;
